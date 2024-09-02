25fc7f96d0473eedbafeca4a52575a47244f48b0
Nazar <nazar.gavrilov20@gmail.com>
2024-09-03 00:59:14 +0300

    Basic handlers (no body)

--- main.go
+++ main.go
@@ -4,6 +4,20 @@ import (
 	"net/http"
 )
 
+func greetingHandler(w http.ResponseWriter, r *http) {
+	/// -> TODO
+}
+
 func main() {
-	
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer) 
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/greeting", greetingHandler)
+
+	fmt.Print("Starting server at port 8080\n")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
