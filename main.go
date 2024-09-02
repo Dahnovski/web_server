86a2778905459aefaee7d62f979059e3fcab7ec0
Nazar <nazar.gavrilov20@gmail.com>
2024-09-03 01:06:31 +0300

    Greeting handler body

--- main.go
+++ main.go
@@ -5,8 +5,25 @@ import (
 )
 
 func greetingHandler(w http.ResponseWriter, r *http) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound) {
+			return
+		}
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello, user!")
+}
+
+func formHandler(w http.ResponseWriter, r *http) {
 	/// -> TODO
 }
+
+/// -> TODO
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
